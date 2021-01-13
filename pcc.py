ac6922020f2f972eb6cd0a7fdba85e6f46de46cf
leadmeout <markhorvat@live.ca>
2021-01-13 18:43:14 +0100

    Update pcc.py

--- pcc.py
+++ pcc.py
@@ -1,13 +1,18 @@
 import random
 
-max_combinations = int(input("How many numbers would you like to enter?\n"))
+max_combinations = int(input("\nHow many numbers would you like to enter?\n\n \
+    \tPlease note that entering any number higher than 7 will result in \n \tcalculations times taking several minutes or more!\n\n \
+    :"))
+
 max_combination_counter = list(range(1, max_combinations+1))
 
 numbers = []
 
 for num in range(1, max_combinations+1):
     number = int(input("Please enter a unique number: "))
-    numbers.append(number)
+    if number not in numbers:
+        numbers.append(number)
+
     
 max_combinations = 1
 
@@ -30,6 +35,8 @@ while count != max_combinations[-1]:
     else:
         continue
 
+print(f"Possible combinations: {max_combinations[-1]}")
+
 for value in possible_combinations.values():
-    print(f"{value}")
+    print(f"{(str(value)[1:-1])}")
 
